38e6948417743441418a5ecb319c2127af16d314
Benjamin Charity <ben@benjamincharity.com>
2016-07-26 18:40:36 -0400

    added tests for no max length

--- src/keypad.controller.spec.js
+++ src/keypad.controller.spec.js
@@ -39,30 +39,70 @@ describe('KeypadController', () => {
 
 
     describe('setNumber', () => {
-        let $scope;
-        let element;
-        let vm;
 
-        beforeEach(() => {
-            $scope = $rootScope.$new();
-            $scope.numbers = '';
-            $scope.neededLength = 4;
-            element = angular.element(
-                '<bc-keypad ' +
-                    'bc-number-model="numbers" ' +
-                    'bc-max-length="{{ neededLength }}"' +
-                '></bc-keypad>'
-            );
-            element = $compile(element)($scope);
-            $scope.$apply();
-            vm = element.isolateScope().vm;
+        describe('test withOUT max length', () => {
+            let $scope;
+            let element;
+            let vm;
+
+            beforeEach(() => {
+                $scope = $rootScope.$new();
+                $scope.numbers = '';
+                element = angular.element(
+                    '<bc-keypad bc-number-model="numbers"></bc-keypad>'
+                );
+                element = $compile(element)($scope);
+                $scope.$apply();
+                vm = element.isolateScope().vm;
+            });
+
+            afterEach(() => {
+                $scope.numbers = '';
+            });
+
+            it('should add to the number model when small', () => {
+                const numberButton = element[0].querySelectorAll('.bc-keypad__button')[2];
+                angular.element(numberButton).triggerHandler('click');
+
+                expect(vm.bcNumberModel).toEqual('3');
+            });
+
+            it('should add to the number model when large', () => {
+                const LONG_NUMBER = '123456789012345678901234567890';
+
+                // Set the model to a long number
+                vm.bcNumberModel = LONG_NUMBER;
+
+                const numberButton = element[0].querySelectorAll('.bc-keypad__button')[2];
+                angular.element(numberButton).triggerHandler('click');
+
+                expect(vm.bcNumberModel).toEqual(LONG_NUMBER + '3');
+            });
         });
 
-        /*
-         *it('should set the number', () => {
-         *    expect(true).toBe(true);
-         *});
-         */
+        describe('test WITH max length', () => {
+            let $scope;
+            let element;
+            let vm;
+
+            beforeEach(() => {
+                $scope = $rootScope.$new();
+                $scope.numbers = '';
+                $scope.neededLength = 4;
+                element = angular.element(
+                    '<bc-keypad ' +
+                        'bc-number-model="numbers" ' +
+                        'bc-max-length="{{ neededLength }}"' +
+                    '></bc-keypad>'
+                );
+                element = $compile(element)($scope);
+                $scope.$apply();
+                vm = element.isolateScope().vm;
+            });
+
+
+
+        });
 
     });
 
