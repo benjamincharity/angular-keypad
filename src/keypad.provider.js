220e0a09ae0187cb1c91fd6e2572e98c00a3898c
Benjamin Charity <ben@benjamincharity.com>
2016-08-02 19:42:54 -0400

    changed provider to get around crazy array sorting issue

--- src/keypad.provider.js
+++ src/keypad.provider.js
@@ -5,35 +5,33 @@ export class KeypadConfig {
     // Define defaults
     constructor() {
 
-        // This should be a .svg
-        this.backspaceTemplate = backspaceTemplate;
-
-        // By default there is no max length
-        // Integer
-        this.maxLength = null;
-
         /* eslint-disable no-magic-numbers */
 
-        // Define the array of numbers that makes up the keypad
-        this.numbers = [1, 2, 3, 4, 6, 5, 7, 8, 9, 0];
-        for (const n of this.numbers) {
-            console.warn(n);
-        }
+        this.keypadDefaults = {
+            // By default there is no max length
+            // Integer
+            backspaceTemplate: backspaceTemplate,
+            // Define the array of numbers that makes up the keypad
+            numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+            // This should be a .svg file
+            maxLength: null,
+        };
 
         /* eslint-enable no-magic-numbers */
+
+        // TODO: convert backspace template to the left/right templates
+        // TODO: add methods to overwrite the defaults
+        // TODO: add docs for methods and changes etc
+
     }
 
 
 
 
     $get() {
-        for (const n of this.numbers) {
-            console.info(n);
-        }
-        return this;
+        return this.keypadDefaults;
     }
 
 
 }
-
 
