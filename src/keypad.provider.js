3db807273bee71ae0a1f04c73f87912fdb00b6a5
Benjamin Charity <ben@benjamincharity.com>
2016-08-04 10:50:44 -0400

    removed old backspace template, changed delete to backspace, formatting

--- src/keypad.provider.js
+++ src/keypad.provider.js
@@ -5,25 +5,29 @@ export class KeypadConfig {
     // Define defaults
     constructor() {
 
-        /* eslint-disable no-magic-numbers */
 
         this.keypadDefaults = {
+
+            /* eslint-disable no-magic-numbers */
+
+            // The array of numbers that makes up the keypad
+            numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+
+            /* eslint-enable no-magic-numbers */
+
             // By default there is no max length
             // Integer
-            backspaceTemplate: backspaceTemplate,
-            // Define the array of numbers that makes up the keypad
-            numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
-            // This should be a .svg file
             maxLength: null,
+
+            // Plug'N'Play button types
             types: [
-                'delete',
+                'backspace',
                 'submit',
             ],
+
         };
 
-        /* eslint-enable no-magic-numbers */
 
-        // TODO: convert backspace template to the left/right templates
         // TODO: add methods to overwrite the defaults
         // TODO: add docs for methods and changes etc
 
